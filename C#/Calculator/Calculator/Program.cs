53ea050da89c44fc03dff1cba3e5ba520b79c744
Abdalhadi-Al-Sakbani <140417960+Abdalhadi-Al-Sakbani@users.noreply.github.com>
2024-06-15 10:41:30 +0300

    Add files via upload

---
+++ C#/Calculator/Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my calculator ^_^");
+            Console.Write("Enter the first number : ");
+            float num1 = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Enter the opertion +\\-\\*\\/ : ");
+            string oper = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            float num2 = Convert.ToSingle(Console.ReadLine());
+
+            float result;
+
+            switch (oper)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine("The result of this athmetic process is : {0}", result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine("The result of this athmetic process is : {0}", result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine("The result of this athmetic process is : {0}", result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine("The result of this athmetic process is : {0}", result);
+                    break;
+                default:
+                    Console.WriteLine("Undefined operetor please try again.");
+                    break;
+            }
+        }
+    }
+}
---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] arr = new int[3, 3];
+        int i, j, sum = 0;
+        for (i=0; i<3; ++i)
+        {
+            for(j=0; j<3; ++j)
+                arr[i,j] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (i = 0; i < 3; ++i) 
+        {
+            Console.Write("x[{0} ", i);
+            Console.Write(", {0}", i);
+            Console.Write("] = {0}\t", arr[i,i]);
+        }
+        for(i = 0;i < 3; ++i)
+        {
+            sum += arr[i,i];
+        }
+        Console.WriteLine("TheSum = {0}", sum);
+    }
+}
